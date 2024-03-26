0c8251138c0c645e94a527d34ede7781eaf9d010
hyungyu <yhg8484@naver.com>
2024-03-27 00:07:39 +0900

    feat: 내 정보 조회 API 추가

--- budget-was/src/main/java/com/hg/budget/was/account/AccountController.java
+++ budget-was/src/main/java/com/hg/budget/was/account/AccountController.java
@@ -1,11 +1,16 @@
 package com.hg.budget.was.account;
 
 import com.hg.budget.application.account.service.AccountCommandService;
+import com.hg.budget.application.account.service.AccountQueryService;
+import com.hg.budget.application.account.service.dto.AccountDto;
 import com.hg.budget.was.account.command.JoinCommand;
+import com.hg.budget.was.account.response.MyInfoResponse;
 import com.hg.budget.was.core.annotation.AccountId;
+import com.hg.budget.was.core.response.OkResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
 
     private final AccountCommandService accountCommandService;
+    private final AccountQueryService accountQueryService;
     private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/join")
@@ -27,5 +33,20 @@ public class AccountController {
     @PostMapping("/login")
     public void login(@AccountId String id) {
         accountCommandService.login(id);
+    }
+
+    @GetMapping("/me")
+    public OkResponse<MyInfoResponse> getMyInfo(@AccountId String id) {
+        final AccountDto account = accountQueryService.findAccount(id);
+        return new OkResponse<>(
+            new MyInfoResponse(
+                account.getId(),
+                account.getNickname(),
+                account.getStatus(),
+                account.getRole(),
+                account.getSignUpDateTime(),
+                account.getSignInDateTime()
+            )
+        );
     }
 }
