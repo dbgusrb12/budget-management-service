b6d14db3f79e295548c934e661e874751c9dd176
hyungyu <yhg8484@naver.com>
2024-03-27 00:07:39 +0900

    feat: 내 정보 조회 API 추가

---
+++ budget-was/src/main/java/com/hg/budget/was/account/response/MyInfoResponse.java
@@ -0,0 +1,5 @@
+package com.hg.budget.was.account.response;
+
+public record MyInfoResponse(String id, String nickname, String status, String role, String signUpDateTime, String signInDateTime) {
+
+}
