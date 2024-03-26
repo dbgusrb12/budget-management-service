16afcea37c11fd9a0f08049cb04d40de9804db1d
hyungyu <yhg8484@naver.com>
2024-03-26 14:34:07 +0900

    feat: account 유저 생성 api 추가 및 기본 web config 설정들 추가

---
+++ budget-was/src/main/java/com/hg/budget/was/core/response/OkResponse.java
@@ -0,0 +1,23 @@
+package com.hg.budget.was.core.response;
+
+import com.hg.budget.application.core.code.ApplicationCode;
+import lombok.Getter;
+
+@Getter
+public class OkResponse<T> {
+
+    public static final OkResponse<Void> OK = new OkResponse<>();
+
+    private final String name = ApplicationCode.SUCCESS.name();
+    private final String code = ApplicationCode.SUCCESS.getCode();
+    private final String message = ApplicationCode.SUCCESS.getMessage();
+    private final T result;
+
+    private OkResponse(T result) {
+        this.result = result;
+    }
+
+    public OkResponse() {
+        this.result = null;
+    }
+}
