9759bbce533032d8bed1a8be7d8b42ec97366e24
hyungyu <yhg8484@naver.com>
2024-03-26 23:04:44 +0900

    refactor: interceptor 에서 처리하던 인증 로직 인터페이스로 분리 후 chaining 형태로 변경

---
+++ budget-was/src/main/java/com/hg/budget/was/core/security/process/AuthenticationProcessing.java
@@ -0,0 +1,25 @@
+package com.hg.budget.was.core.security.process;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public interface AuthenticationProcessing {
+
+    /*
+     * 인증 process 의 order 설정
+     * AuthenticationProcessingManager 에서 해당 order 의 오름차순으로 정렬 후 실행한다.
+     */
+    int getOrder();
+
+    /*
+     * 인증 process 의 진행 여부 설정
+     * 해당 반환값이 false 라면 인증을 시도하지 않고 넘어간다.
+     */
+    boolean requiresAuthentication(HttpServletRequest request, HttpServletResponse response);
+
+    /*
+     * 인증 process 실행
+     */
+    boolean attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws IOException;
+}
---
+++ budget-was/src/main/java/com/hg/budget/was/core/security/process/LoginRequestProcessing.java
@@ -0,0 +1,69 @@
+package com.hg.budget.was.core.security.process;
+
+import com.google.gson.Gson;
+import com.hg.budget.application.core.code.ApplicationCode;
+import com.hg.budget.application.core.exception.ApplicationException;
+import com.hg.budget.was.account.command.LoginCommand;
+import com.hg.budget.was.account.response.LoginResponse;
+import com.hg.budget.was.core.response.OkResponse;
+import com.hg.budget.was.core.security.UserDetails;
+import com.hg.budget.was.core.security.UserDetailsService;
+import com.hg.budget.was.core.security.context.UserDetailsContextHolder;
+import com.hg.budget.was.core.security.jwt.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+public class LoginRequestProcessing implements AuthenticationProcessing {
+
+    private final AntPathRequestMatcher loginRequestMatcher;
+    private final Gson gson;
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public LoginRequestProcessing(
+        String loginProcessUrl,
+        Gson gson,
+        UserDetailsService userDetailsService,
+        PasswordEncoder passwordEncoder,
+        JwtUtil jwtUtil
+    ) {
+        this.loginRequestMatcher = new AntPathRequestMatcher(loginProcessUrl);
+        this.gson = gson;
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public int getOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean requiresAuthentication(HttpServletRequest request, HttpServletResponse response) {
+        return loginRequestMatcher.matches(request);
+    }
+
+    @Override
+    public boolean attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        UserDetailsContextHolder.clearContext();
+        final LoginCommand command = gson.fromJson(request.getReader(), LoginCommand.class);
+        UserDetails loginUser = userDetailsService.loadUserByUsername(command.id());
+        if (!passwordEncoder.matches(command.password(), loginUser.getPassword())) {
+            throw new ApplicationException(ApplicationCode.BAD_CREDENTIALS);
+        }
+        final String accessToken = jwtUtil.issueToken(loginUser);
+        final String responseBody = gson.toJson(new OkResponse<>(new LoginResponse(accessToken)));
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding(StandardCharsets.UTF_8.displayName());
+        response.getWriter().println(responseBody);
+        UserDetailsContextHolder.setContext(loginUser);
+        return true;
+    }
+}
