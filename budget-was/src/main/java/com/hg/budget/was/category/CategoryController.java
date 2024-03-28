9e885c6a4c8af90d6f7ef6247100f8fd5a1335c9
hyungyu <yhg8484@naver.com>
2024-03-28 16:02:24 +0900

    feat: 카테고리 생성 기능 추가

--- budget-was/src/main/java/com/hg/budget/was/category/CategoryController.java
+++ budget-was/src/main/java/com/hg/budget/was/category/CategoryController.java
@@ -1,11 +1,16 @@
 package com.hg.budget.was.category;
 
+import com.hg.budget.application.category.CategoryCommandService;
 import com.hg.budget.application.category.CategoryQueryService;
+import com.hg.budget.was.category.command.CreateCategoryCommand;
 import com.hg.budget.was.category.response.CategoryResponse;
 import com.hg.budget.was.core.response.OkResponse;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/categories")
 public class CategoryController {
 
+    private final CategoryCommandService categoryCommandService;
     private final CategoryQueryService categoryQueryService;
+
+    @PostMapping
+    public void createCategory(@Valid @RequestBody CreateCategoryCommand command) {
+        categoryCommandService.createCategory(command.name());
+    }
 
     @GetMapping
     public OkResponse<List<CategoryResponse>> getCategories() {
