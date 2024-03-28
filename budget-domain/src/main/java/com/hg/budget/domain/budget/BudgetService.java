2cd4a6624266391d89de58504edb16972e6fcc4f
hyungyu <yhg8484@naver.com>
2024-03-28 19:35:26 +0900

    feat: budget 도메인 서비스 작성 및 memory repository 생성

--- budget-domain/src/main/java/com/hg/budget/domain/budget/BudgetService.java
+++ budget-domain/src/main/java/com/hg/budget/domain/budget/BudgetService.java
@@ -1,5 +1,11 @@
 package com.hg.budget.domain.budget;
 
+import com.hg.budget.core.client.DateTimeHolder;
+import com.hg.budget.core.client.IdGenerator;
+import com.hg.budget.domain.account.Account;
+import com.hg.budget.domain.budget.port.BudgetRepository;
+import com.hg.budget.domain.category.Category;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,4 +13,61 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class BudgetService {
 
+    private final IdGenerator idGenerator;
+    private final DateTimeHolder dateTimeHolder;
+    private final BudgetRepository budgetRepository;
+
+    public Budget createBudget(long amount, Category category, Account account) {
+        final Budget duplicatedBudget = findBudget(category, account);
+        if (duplicatedBudget.exist()) {
+            throw new IllegalArgumentException();
+        }
+        final Budget budget = Budget.ofCreated(idGenerator, category, amount, account, dateTimeHolder);
+        budgetRepository.save(budget);
+        return budget;
+    }
+
+    public Budget updateAmount(Long id, long amount) {
+        final Budget budget = findBudget(id);
+        if (budget.notExist()) {
+            return budget;
+        }
+        final Budget updateAmount = budget.updateAmount(amount, dateTimeHolder);
+        budgetRepository.update(updateAmount);
+        return updateAmount;
+    }
+
+    public Budget updateCategory(Long id, Category category) {
+        final Budget budget = findBudget(id);
+        if (budget.notExist()) {
+            return budget;
+        }
+        final Budget updateAmount = budget.updateCategory(category, dateTimeHolder);
+        budgetRepository.update(updateAmount);
+        return updateAmount;
+    }
+
+    public void deleteBudget(Long id) {
+        final Budget budget = findBudget(id);
+        if (budget.notExist()) {
+            return;
+        }
+        budgetRepository.delete(budget);
+    }
+
+    public Budget findBudget(Long id) {
+        return budgetRepository.findById(id);
+    }
+
+    public Budget findBudget(Category category, Account account) {
+        return budgetRepository.findByCategoryAndCreatedUser(category, account);
+    }
+
+    public List<Budget> findBudgets(Account account) {
+        return budgetRepository.findByCreatedUser(account);
+    }
+
+    public List<Budget> findBudgets() {
+        return budgetRepository.findAll();
+    }
 }
