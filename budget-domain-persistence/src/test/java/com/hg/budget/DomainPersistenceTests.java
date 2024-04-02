efc760c3a8ee6314d6c0ff8ba7a421da0ddc950e
hyungyu <yhg8484@naver.com>
2024-04-02 16:19:37 +0900

    fix: domain persistence module test Spring application 클래스 패키지 변경

---
+++ budget-domain-persistence/src/test/java/com/hg/budget/DomainPersistenceTests.java
@@ -0,0 +1,11 @@
+package com.hg.budget;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainPersistenceTests {
+
+    public void contextLoads() {
+
+    }
+}
