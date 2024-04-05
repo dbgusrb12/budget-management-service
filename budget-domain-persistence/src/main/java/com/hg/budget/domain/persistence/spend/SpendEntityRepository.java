f2a43198e06f3d24e34ad4b9220be7d493f6494d
hyungyu <yhg8484@naver.com>
2024-04-05 17:55:41 +0900

    feat: SpendEntity 생성 및 Query DSL 연동, SpendSpecification page,size type 변경

--- budget-domain-persistence/src/main/java/com/hg/budget/domain/persistence/spend/SpendEntityRepository.java
+++ budget-domain-persistence/src/main/java/com/hg/budget/domain/persistence/spend/SpendEntityRepository.java
@@ -2,6 +2,6 @@ package com.hg.budget.domain.persistence.spend;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SpendEntityRepository extends JpaRepository<SpendEntity, Long> {
+public interface SpendEntityRepository extends SpendEntityCustomRepository, JpaRepository<SpendEntity, Long> {
 
 }
