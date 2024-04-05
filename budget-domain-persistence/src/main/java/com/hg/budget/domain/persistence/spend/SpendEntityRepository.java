7833b1211bc21cd88445996ca38e208f8888d9e2
hyungyu <yhg8484@naver.com>
2024-04-05 16:50:05 +0900

    feat: Spend Entity 및 Jpa Repository 생성

---
+++ budget-domain-persistence/src/main/java/com/hg/budget/domain/persistence/spend/SpendEntityRepository.java
@@ -0,0 +1,7 @@
+package com.hg.budget.domain.persistence.spend;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpendEntityRepository extends JpaRepository<SpendEntity, Long> {
+
+}
