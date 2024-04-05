fe267f30a9be9becb9567757b22b7f29e760ae81
hyungyu <yhg8484@naver.com>
2024-04-05 17:54:37 +0900

    feat: queryDSL 의존성 및 설정 추가

--- budget-domain-persistence/src/test/java/com/hg/budget/DomainPersistenceTests.java
+++ budget-domain-persistence/src/test/java/com/hg/budget/DomainPersistenceTests.java
@@ -1,9 +1,17 @@
 package com.hg.budget;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DomainPersistenceTests {
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
+    }
 
     public void contextLoads() {
 
