839a6a5b630b4353d0554bd4f2e5532eea688045
hyungyu <yhg8484@naver.com>
2024-04-08 12:16:41 +0900

    feat: 지출 상세 조회, 페이징 조회 기능 추가

---
+++ budget-application/src/test/java/com/hg/budget/application/spend/SpendQueryServiceTest.java
@@ -0,0 +1,149 @@
+package com.hg.budget.application.spend;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.hg.budget.domain.persistence.account.AccountEntity;
+import com.hg.budget.domain.persistence.account.AccountEntityRepository;
+import com.hg.budget.domain.persistence.category.CategoryEntity;
+import com.hg.budget.domain.persistence.category.CategoryEntityRepository;
+import com.hg.budget.domain.persistence.spend.SpendEntity;
+import com.hg.budget.domain.persistence.spend.SpendEntityRepository;
+import com.hg.budget.domain.spend.SpendService;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class SpendQueryServiceTest {
+
+    @Autowired
+    SpendQueryService spendQueryService;
+    SpendQueryServiceTestHelper testHelper;
+
+    @Autowired
+    SpendEntityRepository spendEntityRepository;
+    @Autowired
+    AccountEntityRepository accountEntityRepository;
+    @Autowired
+    CategoryEntityRepository categoryEntityRepository;
+    @Autowired
+    SpendService spendService;
+
+    @BeforeEach
+    void setUp() {
+        spendEntityRepository.deleteAllInBatch();
+        accountEntityRepository.deleteAllInBatch();
+        categoryEntityRepository.deleteAllInBatch();
+        testHelper = new SpendQueryServiceTestHelper(accountEntityRepository, categoryEntityRepository, spendEntityRepository);
+    }
+
+    @Test
+    @DisplayName("getSpend 로 지출 상세 내역을 조회 할 수 있다.")
+    void getSpendTest() {
+        // given
+        final var account = testHelper.createAccount("hg-yu", "hyungyu");
+        final var category = testHelper.createCategory("식비");
+        testHelper.createSpend(1000L, "메모", LocalDateTime.of(2024, 7, 12, 0, 0, 0), category, account);
+
+        // when
+        final var spend = spendQueryService.getSpend(1L, "hg-yu");
+
+        // then
+        assertThat(spend.id()).isEqualTo(1L);
+        assertThat(spend.amount()).isEqualTo(1000L);
+        assertThat(spend.memo()).isEqualTo("메모");
+        assertThat(spend.category().name()).isEqualTo("식비");
+        assertThat(spend.spentDateTime()).isEqualTo("2024-07-12T00:00");
+        assertThat(spend.spentUser().nickname()).isEqualTo("hyungyu");
+        assertThat(spend.excludeTotal()).isFalse();
+    }
+
+    @Test
+    @DisplayName("pageSpend 로 지출 목록을 페이징해서 조회 할 수 있다.")
+    void pageSpendTest() {
+        // given
+        final var account = testHelper.createAccount("hg-yu", "hyungyu");
+        final var category = testHelper.createCategory("식비");
+        testHelper.createSpend(1000L, "메모", LocalDateTime.of(2024, 7, 12, 0, 0, 0), category, account);
+
+        // when
+        final var spend = spendQueryService.pageSpend(
+            1,
+            5,
+            LocalDateTime.of(2024, 7, 12, 0, 0, 0),
+            LocalDateTime.of(2024, 7, 12, 0, 0, 0),
+            null,
+            null,
+            null,
+            "hg-yu"
+        );
+
+        // then
+        assertThat(spend.getTotalElements()).isEqualTo(1);
+        assertThat(spend.getContent().get(0).id()).isEqualTo(1L);
+        assertThat(spend.getContent().get(0).amount()).isEqualTo(1000L);
+        assertThat(spend.getContent().get(0).memo()).isEqualTo("메모");
+        assertThat(spend.getContent().get(0).category().name()).isEqualTo("식비");
+        assertThat(spend.getContent().get(0).spentDateTime()).isEqualTo("2024-07-12T00:00");
+        assertThat(spend.getContent().get(0).spentUser().nickname()).isEqualTo("hyungyu");
+        assertThat(spend.getContent().get(0).excludeTotal()).isFalse();
+    }
+
+    static class SpendQueryServiceTestHelper {
+
+        private final AccountEntityRepository accountEntityRepository;
+        private final CategoryEntityRepository categoryEntityRepository;
+        private final SpendEntityRepository spendEntityRepository;
+        private long categoryGeneratedId = 1L;
+        private long accountGeneratedId = 1L;
+        private long spendGeneratedId = 1L;
+
+        SpendQueryServiceTestHelper(
+            AccountEntityRepository accountEntityRepository,
+            CategoryEntityRepository categoryEntityRepository,
+            SpendEntityRepository spendEntityRepository
+        ) {
+            this.accountEntityRepository = accountEntityRepository;
+            this.categoryEntityRepository = categoryEntityRepository;
+            this.spendEntityRepository = spendEntityRepository;
+        }
+
+        CategoryEntity createCategory(String name) {
+            final var entity = CategoryEntity.of(categoryGeneratedId++, name);
+            return categoryEntityRepository.save(entity);
+        }
+
+        AccountEntity createAccount(String id, String nickname) {
+            final var entity = AccountEntity.ofUpdate(
+                accountGeneratedId++,
+                id,
+                "PASSWORD",
+                nickname,
+                "LIVED",
+                "ROLE_USER",
+                LocalDateTime.of(2024, 7, 12, 0, 0, 0),
+                LocalDateTime.of(2024, 7, 12, 0, 0, 0)
+            );
+            return accountEntityRepository.save(entity);
+        }
+
+        void createSpend(long amount, String memo, LocalDateTime spentDateTime, CategoryEntity category, AccountEntity account) {
+            final var entity = SpendEntity.of(
+                spendGeneratedId++,
+                category,
+                amount,
+                memo,
+                account,
+                spentDateTime,
+                false
+            );
+
+            spendEntityRepository.save(entity);
+        }
+    }
+}
