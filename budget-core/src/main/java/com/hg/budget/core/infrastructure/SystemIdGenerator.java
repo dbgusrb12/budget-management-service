a8071fbcc28c71b93fe28d0c551af26a897831ba
hyungyu <yhg8484@naver.com>
2024-04-08 11:42:30 +0900

    refactor: 아이디 생성 전략 변경
    - 기존에 음수가 나오던 이슈 수정
    - 16자리 숫자로 제한

--- budget-core/src/main/java/com/hg/budget/core/infrastructure/SystemIdGenerator.java
+++ budget-core/src/main/java/com/hg/budget/core/infrastructure/SystemIdGenerator.java
@@ -1,14 +1,20 @@
 package com.hg.budget.core.infrastructure;
 
 import com.hg.budget.core.client.IdGenerator;
+import java.security.SecureRandom;
 import java.util.Random;
 
 public class SystemIdGenerator implements IdGenerator {
 
-    private static final Random RANDOM = new Random();
+    private static final Random RANDOM = new SecureRandom();
+    private static final long UPPER_LIMIT = (long) Math.pow(10, 16);
 
     @Override
     public Long generateRandomLong() {
-        return RANDOM.nextLong();
+        final long id = RANDOM.nextLong(UPPER_LIMIT);
+        if (id > 0) {
+            return id;
+        }
+        return id * -1;
     }
 }
