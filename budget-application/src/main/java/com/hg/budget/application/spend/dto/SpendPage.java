b1e627c1566a0156398d7434e6705a8519a36e5f
hyungyu <yhg8484@naver.com>
2024-04-08 13:54:52 +0900

    lint: SpendPage, TotalAmount dto 린트 수정

--- budget-application/src/main/java/com/hg/budget/application/spend/dto/SpendPage.java
+++ budget-application/src/main/java/com/hg/budget/application/spend/dto/SpendPage.java
@@ -2,9 +2,6 @@ package com.hg.budget.application.spend.dto;
 
 import com.hg.budget.core.dto.Page;
 
-public record SpendPage(
-    TotalAmount totalAmounts,
-    Page<SpendDto> page
-) {
+public record SpendPage(TotalAmount totalAmounts, Page<SpendDto> page) {
 
 }
--- budget-application/src/main/java/com/hg/budget/application/spend/dto/TotalAmount.java
+++ budget-application/src/main/java/com/hg/budget/application/spend/dto/TotalAmount.java
@@ -3,10 +3,7 @@ package com.hg.budget.application.spend.dto;
 import com.hg.budget.application.category.dto.CategoryDto;
 import java.util.List;
 
-public record TotalAmount(
-    long totalAmount,
-    List<TotalAmountByCategory> totalAmountByCategory
-) {
+public record TotalAmount(long totalAmount, List<TotalAmountByCategory> totalAmountByCategory) {
 
     public record TotalAmountByCategory(CategoryDto category, long totalAmount) {
 
