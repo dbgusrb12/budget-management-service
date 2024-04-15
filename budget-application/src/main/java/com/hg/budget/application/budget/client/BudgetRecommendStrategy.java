a2d0b2c9b8038471d2403db97035a42371bffc24
hyungyu <yhg8484@naver.com>
2024-04-15 11:50:28 +0900

    fix: BudgetRecommendStrategy 주석 추가

--- budget-application/src/main/java/com/hg/budget/application/budget/client/BudgetRecommendStrategy.java
+++ budget-application/src/main/java/com/hg/budget/application/budget/client/BudgetRecommendStrategy.java
@@ -4,6 +4,10 @@ import com.hg.budget.application.budget.client.dto.RecommendBudget;
 import com.hg.budget.domain.budget.Budget;
 import java.util.List;
 
+/*
+ * 예산 추천 전략은 언제든지 변경 될 수 있다고 판단.
+ * 추천하는 전략에 대한 추상화를 통해 확장성을 제공한다.
+ */
 public interface BudgetRecommendStrategy {
 
     List<RecommendBudget> recommend(long totalAmount, List<Budget> budgets);
