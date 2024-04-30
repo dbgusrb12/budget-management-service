4dbe80c58107cf57ad292bdfacc840e0add01a06
hyungyu <yhg8484@naver.com>
2024-04-30 12:33:45 +0900

    feat: 지출 추천 response 객체 추가

--- budget-was/src/main/java/com/hg/budget/was/spend/response/RecommendSpendResponse.java
+++ budget-was/src/main/java/com/hg/budget/was/spend/response/RecommendSpendResponse.java
@@ -1,17 +1,25 @@
 package com.hg.budget.was.spend.response;
 
-import com.hg.budget.application.spend.dto.AmountDto;
+import com.hg.budget.application.spend.dto.RecommendDto;
 import java.util.List;
 
-public record RecommendSpendResponse(long totalAmount, List<AmountResponse> amountByCategories) {
+public record RecommendSpendResponse(long totalAmount, List<RecommendResponse> amountByCategories) {
 
-    public static RecommendSpendResponse from(List<AmountDto> recommendSpendList) {
+    public static RecommendSpendResponse from(List<RecommendDto> recommendSpendList) {
         final long totalAmount = recommendSpendList.stream()
-            .mapToLong(AmountDto::amount)
+            .mapToLong(RecommendDto::amount)
             .sum();
-        final List<AmountResponse> amountResponse = recommendSpendList.stream()
-            .map(AmountResponse::from)
+        final List<RecommendResponse> amountResponse = recommendSpendList.stream()
+            .map(RecommendResponse::from)
             .toList();
         return new RecommendSpendResponse(totalAmount, amountResponse);
+    }
+
+    public record RecommendResponse(SpendCategory category, long totalAmount, String comment) {
+
+        public static RecommendResponse from(RecommendDto amountByCategory) {
+            final SpendCategory category = SpendCategory.from(amountByCategory.category());
+            return new RecommendResponse(category, amountByCategory.amount(), amountByCategory.comment());
+        }
     }
 }
